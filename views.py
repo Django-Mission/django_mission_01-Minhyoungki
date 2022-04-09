8f2fb9e2e9ca8df6d0fa7536a4fbf23ee6c0ce71
Minhyoungki <102710645+Minhyoungki@users.noreply.github.com>
2022-04-10 00:35:27 +0900

    Add files via upload

---
+++ views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.core.handlers.wsgi import WSGIRequest
+
+# Create your views here.
+def calculator(request):
+    # return HttpResponse('계산기 구현 기능 시작입니다. 이게 맞나요?')
+    print(f'request = {request}')
+    print(f'request type = {type(request)}')
+    print(f'request.__dict__{request.__dict__}')
+
+    # 1. 데이터 확인
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+    operators = request.GET.get('operators')
+
+    # 2. 계산
+    if operators == '+':
+        result = int(num1) + int(num2)
+    elif operators == '-':
+        result = int(num1) - int(num2)
+    elif operators == '*':
+        result = int(num1) * int(num2)
+    elif operators == '/':
+        result = int(num1) / int(num2)
+    else:
+        result = 0
+
+    # 3. 응답
+    return render(request, 'calculator.html', {'result': result}) 
+
+# Create your views here.
+
+import random
+def lotto(request):
+    
+    lotto_number = list()
+    for _ in range(7):
+        number = random.randint(1,45)
+        lotto_number.append(number)
+
+    return render(request, 'lotto.html',{'lotto_number': lotto_number})
